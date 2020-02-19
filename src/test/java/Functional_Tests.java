2455d6bbaaa3a2a7e518b900b942a7ad6cb27a8f
pvenkas <santosh.prabhu27@gmail.com>
2020-02-18 22:25:33 -0500

    TestNg
    TestNG programs

---
+++ src/test/java/Functional_Tests.java
@@ -0,0 +1,65 @@
+import org.testng.annotations.*;
+
+public class Functional_Tests {
+
+//    @BeforeSuite
+//    public void SuiteCheck()
+//    {
+//        System.out.println("Before Suite is executing successfully");
+//    }
+//
+//    @BeforeTest
+//    public void beforeTest()
+//    {
+//
+//        System.out.println("Before Test is executing successfully");
+//    }
+//    @BeforeMethod
+//    public void bMethod()
+//    {
+//
+//        System.out.println("Before method");
+//    }
+    @Test
+    public void Func_Test1()
+    {
+
+        assert true;
+    }
+    @Test
+    public void Func_Test2()
+    {
+
+        assert true;
+    }
+    @Test
+    public void Func_Test3()
+    {
+
+        assert true;
+    }
+    @Test
+    public void Func_Test4()
+    {
+
+        assert true;
+    }
+    @Test
+    public void Func_Test5()
+    {
+
+        assert true;
+    }
+
+//    @AfterSuite
+//    public void afterSuite()
+//    {
+//        System.out.println("After suite is executing successfully");
+//    }
+//
+//    @AfterTest
+//    public void afterTest()
+//    {
+//        System.out.println("After Test is executing successfully");
+//    }
+}
---
+++ src/test/java/Regression_Tests.java
@@ -0,0 +1,53 @@
+import org.testng.annotations.*;
+
+public class Regression_Tests {
+
+//    @BeforeSuite
+//    public void beforeSuite()
+//    {
+//        System.out.println("Regression: Before Suite");
+//
+//    }
+//
+//    @BeforeTest
+//    public void beforeTest()
+//    {
+//        System.out.println("Before Test");
+//
+//    }
+//
+//    @BeforeMethod
+//    public void beforeMethod()
+//    {
+//        System.out.println("Before Method");
+//    }
+
+    @Test
+    public void regressionTest1()
+    {
+        assert true;
+    }
+
+    @Test
+    public void regressionTest2()
+    {
+        assert true;
+    }
+
+//    @AfterMethod
+//    public void aftermethod()
+//    {
+//        System.out.println("After method");
+//    }
+//
+//    @AfterTest
+//    public void afterTest()
+//    {
+//        System.out.println("After Test");
+//    }
+//    @AfterSuite
+//    public void afterSuite()
+//    {
+//        System.out.println("After Suite");
+//    }
+}
